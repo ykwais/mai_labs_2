b2e4bceac77d5621c10d6d8cbbbf5da05d7f7d39
ykwais <fedorkovaleks04@gmail.com>
2023-10-15 02:11:53 +0300

    -

--- lab2_1/main.c
+++ lab2_1/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
+    printf("Hello, nigga!\n");
     printf("Hello, World!\n");
+    printf("Hello, Tima!\n");
     printf("Hello, World!\n");
-    printf("Hello, World!\n");
+
 
 
     return 0;
