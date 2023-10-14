1a270cbdea22467a581daf32d32a662d2796a913
ykwais <fedorkovaleks04@gmail.com>
2023-10-15 02:35:08 +0300

    -

---
+++ lab2_7/cmake-build-debug/CMakeFiles/lab2_7.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_7.dir/main.c.obj"
+  "CMakeFiles/lab2_7.dir/main.c.obj.d"
+  "lab2_7.exe"
+  "lab2_7.exe.manifest"
+  "lab2_7.pdb"
+  "liblab2_7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab2_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
