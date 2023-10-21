869882122a93a1cf90bd99be058f32ccdbd69d68
ykwais <fedorkovaleks04@gmail.com>
2023-10-21 04:41:39 +0300

    -

--- lab2_9/main.c
+++ lab2_9/main.c
@@ -3,7 +3,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+void sim(int num) {
+    int del = 2;
 
+    while (num > 1) {
+        if (num % del == 0) {
+            printf("%d ", del);
+            num = num / del;
+        } else {
+            del++;
+        }
+    }
+}
 
 int* simple_mult(long long number, int* length)
 {
@@ -61,6 +72,20 @@ long long get_denominator(double number)
     return denominator;
 }
 
+void comparison_array_int(int* main, int size1, int* supp, int size2, int pos){
+    if(size2 > size1){
+        printf("the decimal fraction at this position: %d  -  is not a finite\n", pos+1);
+        return;
+    }
+    for(int i = 0 ; i < size2; ++i){
+        if(supp[i] == 1 && main[i] != 1){
+            printf("the decimal fraction at this position: %d  -  is not a finite\n", pos+1);
+            return;
+        }
+    }
+    printf("the decimal fraction at this position: %d  -  is a finite\n", pos+1);
+}
+
 void final_representation(long long base, int count, ...)
 {
     //set от base
@@ -73,10 +98,10 @@ void final_representation(long long base, int count, ...)
         double current = va_arg(ptr, double);
         long long den = get_denominator(current);
         int* mult_current = simple_mult(den, &length_current);
-
+        comparison_array_int(mult_base, length_base, mult_current, length_current, i);
         length_current = 3;
         free(mult_current);
-        //printf("%lld\n", den);
+
     }
     va_end(ptr);
     free(mult_base);
@@ -85,16 +110,11 @@ void final_representation(long long base, int count, ...)
 
 
 int main() {
-    final_representation(10, 5, 0.45, 0.125, 0.12, 0.50, 0.8125);
-//    simplify(997);
-    int len = 3;
-    int* ans = simple_mult(81, &len);
+    final_representation(36, 5, 0.45, 0.125, 0.12, 0.50, 0.8125);
+
+    sim(36);
     printf("\n");
-    printf("\n");
-    for(int i = 0; i < len; ++i){
-        printf("%d  ", ans[i]);
-    }
-    //printf("\n%d\n", ans[len+6]);
-    printf("\n");
+    sim(25);
+
 
 }
