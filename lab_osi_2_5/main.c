ea0a22c1dd6233cd02ca1bbf252d8a0e59ca53ae
ykwais <fedorkovaleks04@gmail.com>
2023-10-28 19:15:12 +0300

    -

--- lab_osi_2_5/main.c
+++ lab_osi_2_5/main.c
@@ -2,88 +2,79 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdbool.h>
 #include <unistd.h>
 
-#define COUNT 3
+#define N 5
 
-#define WAITING 0
+#define QUEUE 0
 #define INSIDE 1
-#define RELAX 2
-#define ENTERING 3
+#define NOWANT 2
 
-bool inside_man = false;
-bool inside_women = false;
 
-int N = 0;
-int MaxN = 0;
 
-sem_t men[COUNT];
-sem_t women[COUNT];
+int max_count = 0;
 
-int state_men[COUNT];
-int state_women[COUNT];
+sem_t sem_men[N];
+sem_t sem_women[N];
+
+int state_men[N];
+int state_women[N];
 
 sem_t mutex;
 
-void try_enter_man(int i)
+void m_want_shower(int k)
 {
     int n = 0;
-    for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < N; i++)
     {
         if (state_women[i] == INSIDE)
         {
             return;
         }
-    }
-    //printf("Empty or only men\n");
-    for (int i = 0; i < COUNT; i++)
-    {
         if (state_men[i] == INSIDE)
         {
             n++;
         }
     }
-    if (state_men[i] == WAITING && n < MaxN)
+
+    
+    if (state_men[k] == QUEUE && n < max_count)
     {
-        state_men[i] = INSIDE;
-        sem_post(&men[i]);
+        state_men[k] = INSIDE;
+        sem_post(&sem_men[k]);
     }
 }
 
 
 
-void try_enter_woman(int i)
+void w_want_shower(int k)
 {
     int n = 0;
-    for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < N; i++)
     {
         if (state_men[i] == INSIDE)
         {
             return;
         }
-    }
-    //printf("Empty or only women\n");
-    for (int i = 0; i < COUNT; i++)
-    {
         if (state_women[i] == INSIDE)
         {
             n++;
         }
     }
-    if (state_women[i] == WAITING && n < MaxN)
+
+    if (state_women[k] == QUEUE && n < max_count)
     {
-        state_women[i] = INSIDE;
-        sem_post(&women[i]);
+        state_women[k] = INSIDE;
+        sem_post(&sem_women[k]);
     }
 }
 
-void pass_post()
+void another()
 {
-    for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < N; i++)
     {
-        try_enter_woman(i);
-        try_enter_man(i);
+        w_want_shower(i);
+        m_want_shower(i);
     }
 }
 
@@ -92,12 +83,12 @@ void woman_wants_to_enter(int i)
 {
     sem_wait(&mutex);
 
-    state_women[i] = WAITING;
-    pass_post();
+    state_women[i] = QUEUE;
+    another();
 
     sem_post(&mutex);
 
-    sem_wait(&women[i]);
+    sem_wait(&sem_women[i]);
 }
 
 
@@ -105,8 +96,8 @@ void woman_leaves(int i)
 {
     sem_wait(&mutex);
 
-    state_women[i] = RELAX;
-    pass_post();
+    state_women[i] = NOWANT;
+    another();
 
     sem_post(&mutex);
 }
@@ -119,12 +110,12 @@ void man_wants_to_enter(int i)
 {
     sem_wait(&mutex);
 
-    state_men[i] = WAITING;
-    pass_post();
+    state_men[i] = QUEUE;
+    another();
 
     sem_post(&mutex);
 
-    sem_wait(&men[i]);
+    sem_wait(&sem_men[i]);
 }
 
 
@@ -132,102 +123,106 @@ void man_leaves(int i)
 {
     sem_wait(&mutex);
 
-    state_men[i] = RELAX;
-    pass_post();
+    state_men[i] = NOWANT;
+    another();
 
     sem_post(&mutex);
 }
 
 
-void *real_man(void *id)
+void *man(void *id)
 {
     int i = *((int*)id);
-    while (true)
+    while (1)
     {
-        //printf("Наслаждается жизнью парень %d\n", i+1);
-        sleep(rand() % 10);
 
-        printf("Кун %d решил  занять душ\n", i+1);
+        sleep(rand()%5);
+
+        printf("M %d queue\n", i+1);
         man_wants_to_enter(i);
 
-        printf("Кун %d в душе\n", i+1);
-        sleep(rand() % 10); // Чилит
+        printf("M %d in\n", i+1);
+        sleep(rand()%5);
 
-        printf("Кун %d вышел\n", i+1);
+        printf("M %d out\n", i+1);
         man_leaves(i);
-        sleep(rand() % 10);
+        sleep(rand()%5);
     }
 }
 
-void *real_woman(void *id)
+void *woman(void *id)
 {
     int i = *((int*)id);
-    while (true)
+    while (1)
     {
-        //printf("Тян %d грустит\n", i+1);
-        sleep(rand() % 10);
 
-        printf("Тян %d решила занять душ\n", i+1);
+        sleep(rand()%5);
+
+        printf("W %d queue\n", i+1);
         woman_wants_to_enter(i);
 
-        printf("Тян %d в душе\n", i+1);
-        sleep(rand() % 10); // долга
+        printf("W %d inside\n", i+1);
+        sleep(rand()%5);
 
-        printf("Тян %d вышла\n", i+1);
+        printf("W %d out\n", i+1);
         woman_leaves(i);
-        sleep(rand() % 20);
+        sleep(rand()%5);
     }
 }
 
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
     if (argc != 2) {
         printf("Usage: %s <N>\n", argv[0]);
         return 1;
     }
 
+    max_count = atoi(argv[1]);
+    pthread_t threads[N*2];
+
     if (sem_init(&mutex, 0, 1) == -1) {
-        perror("Ошибка инициализации семафора");
+        printf("trouble semafore init\n");
         return 1;
     }
 
-    for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < N; i++)
     {
-        if (sem_init(&men[i], 0, 0) == -1) {
-            perror("Ошибка инициализации семафора");
+        if (sem_init(&sem_men[i], 0, 0) == -1) {
+            printf("trouble semafore init\n");
             return 1;
         }
 
-        if (sem_init(&women[i], 0, 0) == -1) {
-            perror("Ошибка инициализации семафора");
+        if (sem_init(&sem_women[i], 0, 0) == -1) {
+            printf("trouble semafore init\n");
             return 1;
         }
 
-        state_men[i] = RELAX;
-        state_women[i] = RELAX;
+        state_men[i] = NOWANT;
+        state_women[i] = NOWANT;
     }
 
-    MaxN = atoi(argv[1]); // Maximum number of people
 
-    pthread_t threads[COUNT*2];
-    int men_indices[COUNT];
-    int women_indices[COUNT];
 
-    for (int i = 0; i < COUNT; i++)
+
+    int men_indices[N];
+    int women_indices[N];
+
+    for (int i = 0; i < N; i++)//init
     {
         men_indices[i] = i;
         women_indices[i] = i;
 
-        pthread_create(&threads[i], NULL, real_woman, &women_indices[i]);
-        pthread_create(&threads[COUNT+i], NULL, real_man, &men_indices[i]);
+        pthread_create(&threads[i], NULL, woman, &women_indices[i]);
+        pthread_create(&threads[N+i], NULL, man, &men_indices[i]);
     }
 
-    for (int i = 0; i < COUNT; i++)
+    for (int i = 0; i < N; i++)//finish
      {
 
         pthread_join(threads[i], NULL);
-        pthread_join(threads[COUNT+i], NULL);
+        pthread_join(threads[N+i], NULL);
     }
 
     return 0;
